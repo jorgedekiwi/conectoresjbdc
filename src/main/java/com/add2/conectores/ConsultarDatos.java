77f85d1bffce0766c9b6db3e88b9139fe2d85677
jorge <jorgepinto1998@hotmail.com>
2021-10-29 11:36:11 +0200

    Primer commit

---
+++ src/main/java/com/add2/conectores/ConsultarDatos.java
@@ -0,0 +1,19 @@
+package com.add2.conectores;
+import java.sql.*;
+public class ConsultarDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jugadores","root","clasedam1");
+			Statement stmt= con.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from alumnos");
+			while(rs.next())
+				System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
+			con.close();
+		}catch (SQLException e) {
+			System.out.print(e);
+		}
+	}
+
+}
