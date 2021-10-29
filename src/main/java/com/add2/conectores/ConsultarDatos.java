d9aa310ce75e3be84027999a0ab60f8233c21a15
jorge <jorgepinto1998@hotmail.com>
2021-10-29 11:43:33 +0200

    Tercer commit

--- src/main/java/com/add2/conectores/ConsultarDatos.java
+++ src/main/java/com/add2/conectores/ConsultarDatos.java
@@ -5,6 +5,7 @@ public class ConsultarDatos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("SIU");
+		System.out.print("SIU2");
 		try {
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jugadores","root","clasedam1");
 			Statement stmt= con.createStatement();
